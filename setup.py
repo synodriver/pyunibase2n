8510ec8fbcfe170b9e38c5a935203788fd809b4d
synodriver <diguohuangjiajinweijun@gmail.com>
2022-10-20 15:42:50 +0000

    now can build

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+# because the name is not libawesome.so,
+# ```extra_link_args``` is needed for gcc
+# yet cgo only supports gcc,
+ext_modules = [
+    Extension("unibase2n._b2n",
+              sources=["unibase2n/_b2n.pyx"],
+              include_dirs=["."],
+              library_dirs=["."],
+              extra_objects=["./libunibase.a"],
+              runtime_library_dirs=["."])]
+setup(
+    name='go',
+    ext_modules=cythonize(ext_modules,compiler_directives={"cdivision": True,
+                                                   "embedsignature": True,
+                                                   "boundscheck": False,
+                                                   "wraparound": False}),
+    zip_safe=False,
+)
--- unibase.go
+++ unibase.go
@@ -4,182 +4,184 @@ import "github.com/fumiama/unibase2n"
 
 import "C"
 
-// export EncodeBase8192
+//export EncodeBase8192
 func EncodeBase8192(inp []byte) []byte {
 	return unibase2n.Base8192.Encode(inp)
 }
 
-// export DecodeBase8192
+//export DecodeBase8192
 func DecodeBase8192(inp []byte) []byte {
 	return unibase2n.Base8192.Decode(inp)
 }
 
-// export EncodeBase4096
+//export EncodeBase4096
 func EncodeBase4096(inp []byte) []byte {
 	return unibase2n.Base4096.Encode(inp)
 }
 
-// export DecodeBase4096
+//export DecodeBase4096
 func DecodeBase4096(inp []byte) []byte {
 	return unibase2n.Base4096.Decode(inp)
 }
 
-// export EncodeBase512
+//export EncodeBase512
 func EncodeBase512(inp []byte) []byte {
 	return unibase2n.Base512.Encode(inp)
 }
 
-// export DecodeBase512
+//export DecodeBase512
 func DecodeBase512(inp []byte) []byte {
 	return unibase2n.Base512.Decode(inp)
 }
 
-// export EncodeBase256
+//export EncodeBase256
 func EncodeBase256(inp []byte) []byte {
 	return unibase2n.Base256.Encode(inp)
 }
 
-// export DecodeBase256
+//export DecodeBase256
 func DecodeBase256(inp []byte) []byte {
 	return unibase2n.Base256.Decode(inp)
 }
 
-// export EncodeBaseMath
+//export EncodeBaseMath
 func EncodeBaseMath(inp []byte) []byte {
 	return unibase2n.BaseMath.Encode(inp)
 }
 
-// export DecodeBaseMath
+//export DecodeBaseMath
 func DecodeBaseMath(inp []byte) []byte {
 	return unibase2n.BaseMath.Decode(inp)
 }
 
-// export EncodeBaseTanWi
+//export EncodeBaseTanWi
 func EncodeBaseTanWi(inp []byte) []byte {
 	return unibase2n.BaseTanWi.Encode(inp)
 }
 
-// export DecodeBaseTanWi
+//export DecodeBaseTanWi
 func DecodeBaseTanWi(inp []byte) []byte {
 	return unibase2n.BaseTanWi.Decode(inp)
 }
 
-// export EncodeBase128
+//export EncodeBase128
 func EncodeBase128(inp []byte) []byte {
 	return unibase2n.Base128.Encode(inp)
 }
 
-// export DecodeBase128
+//export DecodeBase128
 func DecodeBase128(inp []byte) []byte {
 	return unibase2n.Base128.Decode(inp)
 }
 
-// export EncodeBaseDevanagari
+//export EncodeBaseDevanagari
 func EncodeBaseDevanagari(inp []byte) []byte {
 	return unibase2n.BaseDevanagari.Encode(inp)
 }
 
-// export DecodeBaseDevanagari
+//export DecodeBaseDevanagari
 func DecodeBaseDevanagari(inp []byte) []byte {
 	return unibase2n.BaseDevanagari.Decode(inp)
 }
 
-// export EncodeBase64
+//export EncodeBase64
 func EncodeBase64(inp []byte) []byte {
 	return unibase2n.Base64.Encode(inp)
 }
 
-// export DecodeBase64
+//export DecodeBase64
 func DecodeBase64(inp []byte) []byte {
 	return unibase2n.Base64.Decode(inp)
 }
 
-// export EncodeBase64Gua
+//export EncodeBase64Gua
 func EncodeBase64Gua(inp []byte) []byte {
 	return unibase2n.Base64Gua.Encode(inp)
 }
 
-// export DecodeBase64Gua
+//export DecodeBase64Gua
 func DecodeBase64Gua(inp []byte) []byte {
 	return unibase2n.Base64Gua.Decode(inp)
 }
 
-// export EncodeBaseRune
+//export EncodeBaseRune
 func EncodeBaseRune(inp []byte) []byte {
 	return unibase2n.BaseRune.Encode(inp)
 }
 
-// export DecodeBaseRune
+//export DecodeBaseRune
 func DecodeBaseRune(inp []byte) []byte {
 	return unibase2n.BaseRune.Decode(inp)
 }
 
-// export EncodeBaseMongolian
+//export EncodeBaseMongolian
 func EncodeBaseMongolian(inp []byte) []byte {
 	return unibase2n.BaseMongolian.Encode(inp)
 }
 
-// export DecodeBaseMongolian
+//export DecodeBaseMongolian
 func DecodeBaseMongolian(inp []byte) []byte {
 	return unibase2n.BaseMongolian.Decode(inp)
 }
 
-// export EncodeBaseBox
+//export EncodeBaseBox
 func EncodeBaseBox(inp []byte) []byte {
 	return unibase2n.BaseBox.Encode(inp)
 }
 
-// export DecodeBaseBox
+//export DecodeBaseBox
 func DecodeBaseBox(inp []byte) []byte {
 	return unibase2n.BaseBox.Decode(inp)
 }
 
-// export EncodeBase32
+//export EncodeBase32
 func EncodeBase32(inp []byte) []byte {
 	return unibase2n.Base32.Encode(inp)
 }
 
-// export DecodeBase32
+//export DecodeBase32
 func DecodeBase32(inp []byte) []byte {
 	return unibase2n.Base32.Decode(inp)
 }
 
-// export EncodeBaseTibetan
+//export EncodeBaseTibetan
 func EncodeBaseTibetan(inp []byte) []byte {
 	return unibase2n.BaseTibetan.Encode(inp)
 }
 
-// export DecodeBaseTibetan
+//export DecodeBaseTibetan
 func DecodeBaseTibetan(inp []byte) []byte {
 	return unibase2n.BaseTibetan.Decode(inp)
 }
 
-// export EncodeBase16
+//export EncodeBase16
 func EncodeBase16(inp []byte) []byte {
 	return unibase2n.Base16.Encode(inp)
 }
 
-// export DecodeBase16
+//export DecodeBase16
 func DecodeBase16(inp []byte) []byte {
 	return unibase2n.Base16.Decode(inp)
 }
 
-// export EncodeBaseBuginese
+//export EncodeBaseBuginese
 func EncodeBaseBuginese(inp []byte) []byte {
 	return unibase2n.BaseBuginese.Encode(inp)
 }
 
-// export DecodeBaseBuginese
+//export DecodeBaseBuginese
 func DecodeBaseBuginese(inp []byte) []byte {
 	return unibase2n.BaseBuginese.Decode(inp)
 }
 
-// export EncodeBase8Gua
+//export EncodeBase8Gua
 func EncodeBase8Gua(inp []byte) []byte {
 	return unibase2n.Base8Gua.Encode(inp)
 }
 
-// export DecodeBase8Gua
+//export DecodeBase8Gua
 func DecodeBase8Gua(inp []byte) []byte {
 	return unibase2n.Base8Gua.Decode(inp)
 }
+
+func main() {}
